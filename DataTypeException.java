32d1203d5eae4f181987d78a24a618a5c81386aa
MalenferFrncs <106729850+MalenferFrncs@users.noreply.github.com>
2022-10-31 15:18:19 +0100

    Add files via upload

---
+++ DataTypeException.java
@@ -0,0 +1,6 @@
+public class DataTypeException extends Exception{
+    
+    public DataTypeException(){
+        super("le type de donnés de la trame n'est pas traité");
+    }
+}
---
+++ Trame.java
@@ -0,0 +1,64 @@
+import java.io.FileReader;
+
+
+
+public class Trame {
+    private Champ preambule, destination,source,type,crc,data;
+    
+   
+    Trame(FileReader r){
+        try{
+            preambule = new Champ(r,8,"preambule"); 
+            destination = new Champ(r, 6, "destination");
+            source = new Champ(r,6,"source");
+            type = new Champ(r,2,"champ");
+            //TODO initialiser le champ data
+            crc = new Champ(r,4,"CRC");
+
+
+        }catch(WrongFileTypeException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    public String getDataType() throws DataTypeException{
+        String s = type.getOctet();
+        switch (s){
+            case "0800" : return "IP";
+            case "0806" : return "ARP";
+            default : throw new DataTypeException();
+        } 
+    }
+
+    private String getMacAdr(Champ c){
+        String s = c.getOctet();
+        String r = "";
+        int i = 1;
+            while(i<s.length()){
+                r+=s.charAt(i);
+                if(i%2==0){
+                    r+=':';
+                }
+            }
+    
+        return r;
+    }
+
+    public String getAdrDest(){
+        return getMacAdr(destination);
+    }
+
+    public String getAdrSource(){
+        return getMacAdr(source);
+    }
+
+    public String getOctet(){
+        return preambule.getOctet()+destination.getOctet()+source.getOctet()+type.getOctet()+data.getOctet()+crc.getOctet();
+    }
+
+    
+
+
+    
+}
