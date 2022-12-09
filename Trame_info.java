c07f029623ae4ed249f7d37f8880c6b45814aabb
tkqdldk <41846319+tkqdldk@users.noreply.github.com>
2022-12-09 14:28:41 +0100

    Add files via upload

--- Trame_info.java
+++ Trame_info.java
@@ -16,18 +16,18 @@ public class Trame_info{
     public String envoiData(){
         String affichage ="";
         affichage += "ip : "+ip.getIpSource()+ " port : "+tcp.get_src_port();
-        affichage +=  " --> " ; 
+        affichage +=  " --> " ;
         affichage += "ip : "+ip.getIpDestionation() +" port : "+ tcp.get_dest_port();
-        
+
         affichage += " info transimissions : ";
-        
+
         if(!isHttp()){
-            
+
             affichage += "TCP : "+tcp.getflagsAffichage();
-            affichage += " win : "+tcp.getWindow()+" len : "+tcp.getTailleData()+"\n";
+            affichage += " win : "+tcp.getWindow()+" len : "+tcp.getTailleData()+"seq :"+tcp.get_seq_num()+"ack :"+tcp.get_ack_num()+"\n";
         } else { affichage += "HTTP : "+ http.getRequette()+"\n";}
 
-       
+
         return affichage ;
 
     }
@@ -35,26 +35,26 @@ public class Trame_info{
     public String recoitData(){
         String affichage ="";
         affichage += "ip : "+ip.getIpDestionation()+" port : "+ tcp.get_dest_port();
-        affichage +=  " <-- " ; 
+        affichage +=  " <-- " ;
         affichage += "ip : "+ip.getIpSource() +" port : "+ tcp.get_src_port();
         affichage += " info transimissions : ";
         if(!isHttp()){
             affichage += "TCP : "+tcp.getflagsAffichage();
-            affichage += " win : "+tcp.getWindow()+" len : "+tcp.getTailleData()+"\n";
+            affichage += " win : "+tcp.getWindow()+" len : "+tcp.getTailleData()+"seq :"+tcp.get_seq_num()+"ack :"+tcp.get_ack_num()+"\n";
         } else { affichage += "HTTP : "+ http.getRequette() + "\n";}
-       
+
         return affichage ;
-       
+
     }
 
     private boolean isHttp(){
         String s = http.getRequette();
-        
+
         if (s.equals("")){return false;}
         else{
             String str =s.substring(0, 1);
             if(str.equals("H")){if (s.substring(1, 2).equals("T")){return false;}
-                                else {return true;} } 
+                                else {return true;} }
             if(str.equals("G")){return true;}
             if(str.equals("D")){return true;}
             if(str.equals("H")){return true;}
@@ -62,5 +62,3 @@ public class Trame_info{
         }
     }
 }
-
-
